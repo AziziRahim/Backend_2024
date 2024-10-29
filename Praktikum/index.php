44015cf830db1c6b0258299611cc4c300884bd93
Azizi Rahim <azizirahim67@gmail.com>
2024-10-29 11:25:20 +0700

    PRAKTIKUM

---
+++ Praktikum/index.php
@@ -0,0 +1,39 @@
+<?php
+
+// Variabel
+ $nama = "azizi";
+ $jurusan = "teknik informatika";
+
+//  tipe daata
+
+/**
+ * string 
+ * integer
+ * float
+ * boolean
+ */
+
+//  $name = "L"
+//  $nilai = 90; 
+//  $phi = 3.14
+//  $true = True
+//  $false = False 
+
+//  percabangan 
+
+// perulangan
+
+// for loop
+echo(1);
+echo(2);
+echo(3);
+
+// oop
+
+class Fruit{
+    public $name;
+    public $warma;
+    public $rasa;
+}
+
+$fruit1;
